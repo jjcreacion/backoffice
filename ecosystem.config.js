97476f87b4ef105d3d2873e3a37ea6d84b14d28e
Johann <jjcreacion@gmail.com>
2025-12-06 11:25:17 -0400

    Ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: 'npm',
     args: 'start', 
     exec_mode: 'cluster',
-    instances: max, 
+    instances: 'max', 
     out_file: '/var/www/backoffice/logs/out.log',
     error_file: '/var/www/backoffice/logs/error.log',
 
