3764dd9325a959d1de767d13d4dd8b944e8dd5d5
Johann <jjcreacion@gmail.com>
2025-12-08 07:42:52 -0400

    [Bug][Login] Error de aut

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -132,10 +132,12 @@ class AuthService {
     axios.interceptors.response.use(
       (response) => response,
       (error) => {
-        if (error.response?.status === 401) {
-          this.logout()
-          window.location.href = '/'
-        }
+          if (error.response?.status === 401) {
+            this.logout()
+            if (typeof window !== 'undefined') {
+            window.location.href = '/'
+          }
+       }
         return Promise.reject(error)
       }
     )
